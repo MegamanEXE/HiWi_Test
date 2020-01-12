ce399188427d399ab04393157ef97ca15dcd5eff
Hamza Masud <MegamanEXE@live.com>
2020-01-12 11:42:48 +0100

    First commit
    Main files

---
+++ Assets/MoveBone.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBone : MonoBehaviour
+{
+    private bool direction;
+     // Use this for initialization
+     void Start()
+     {
+         direction = true;
+         StartCoroutine(RotateObject(60, Vector3.right, 1));
+     }
+ 
+     IEnumerator RotateObject(float angle, Vector3 axis, float inTime)
+     {
+         // calculate rotation speed         
+         float rotationSpeed = angle / inTime;
+         
+         while (true)
+         {
+             // save starting rotation position
+             Quaternion startRotation = transform.rotation;
+             
+             float deltaAngle = 0;
+ 
+             // rotate until reaching angle
+             while (deltaAngle < angle)
+             {
+                     deltaAngle += rotationSpeed * Time.deltaTime;
+                     deltaAngle = Mathf.Min(deltaAngle, angle);
+                if (direction)
+                     transform.rotation = startRotation * Quaternion.AngleAxis(deltaAngle, axis);
+                else 
+                    transform.rotation = startRotation * Quaternion.AngleAxis(-1*deltaAngle, axis);
+                    
+                 yield return null;
+             }
+             direction = !direction;  
+         }
+     }
+ }
+
---
+++ Assets/changeColor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class changeColor : MonoBehaviour
+{
+    Renderer rend;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyUp(KeyCode.Alpha1))  rend.material.SetColor("_Color", Color.red);
+        if(Input.GetKeyUp(KeyCode.Alpha2)) rend.material.SetColor("_Color", Color.green);
+        if (Input.GetKeyUp(KeyCode.Alpha3)) rend.material.SetColor("_Color", Color.blue);
+    }
+}
